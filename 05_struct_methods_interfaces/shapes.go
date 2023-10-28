ed7afd4d2edaf5a710c1e48561303b910ae3bca2
ashwnacharya <b.ashwin.acharya@gmail.com>
2023-10-28 13:46:18 +0200

    Adds 5th lesson. Structs, methods and interfaces.

---
+++ 05_struct_methods_interfaces/shapes.go
@@ -0,0 +1,40 @@
+package struct_methods_interfaces
+
+import (
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+func (rectangle Rectangle) Perimeter() float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (rectangle Rectangle) Area() float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (circle Circle) Area() float64 {
+	return math.Pi * circle.Radius * circle.Radius
+}
+
+
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func (triangle Triangle) Area() float64 {
+	return triangle.Base * triangle.Height * 0.5
+}
