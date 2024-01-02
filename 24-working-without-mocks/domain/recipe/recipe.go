d75d5d036ff07ab8808501101d347ea06fc9e466
ashwnacharya <b.ashwin.acharya@gmail.com>
2024-01-02 02:01:27 +0100

    Chapter 24: Adds meal type

--- 24-working-without-mocks/domain/recipe/recipe.go
+++ 24-working-without-mocks/domain/recipe/recipe.go
@@ -2,8 +2,23 @@ package recipe
 
 import "github.com/ashwnacharya/working-without-mocks/domain/ingredients"
 
+
+type MealType int
+
+const (
+	Breakfast MealType = iota
+	Lunch
+	Dinner
+)
+
+func (m MealType) String() string {
+	return [...]string{"Breakfast", "Lunch", "Dinner"}[m]
+}
+
+
 type Recipe struct {
 	Name string
+	MealType MealType
 	Ingredients []ingredients.Ingredient
 }
 
